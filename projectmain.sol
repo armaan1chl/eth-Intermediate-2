432f8107c672ae5a206ee164b0ea494d979d7d8b
ARMAAN CHAHAL <132256505+armaan1chl@users.noreply.github.com>
2024-06-11 17:52:58 +0530

    Update projectmain.sol

--- projectmain.sol
+++ projectmain.sol
@@ -1,1 +1,20 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
+contract SimpleContract {
+    uint public number;
+    string public message;
+    
+    constructor() {
+        number = 0;
+        message = "Hello, My name is Armaan Chahal";
+    }
+    
+    function setNumber(uint _newNumber) public {
+        number = _newNumber;
+    }
+    
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
